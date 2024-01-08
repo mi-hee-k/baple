50c510fc634b5365db08a3624dd5dbb0c9d9416e
hyewon-han <hw1635@hanmail.net>
2024-01-08 21:31:59 +0900

    update : 헤더 푸터 ui 작업 (#4)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,18 @@
-import React, { ReactNode } from "react";
-import Header from "./layout/Header";
-import Footer from "./layout/Footer";
+import React, { ReactNode } from 'react';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+
 type Props = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
   return (
-    <>
+    <div className='flex flex-col justify-between h-[100vh]'>
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
 const Header = () => {
-  return <header className="z-20 top-0 w-screen bg-green-800">Header</header>;
+  return (
+    <header className='bg-yellow-300 py-2 font-bold mb-8 sticky top-0 z-20 shadow-md'>
+      <div className='container m-auto flex flex-row items-center max-w-[1200px] min-h-[48px] w-[90%]'>
+        <nav className='flex flex-row gap-6'>
+          <Link href='/'>BAPLE</Link>
+          <Link href='/nearby'>주변 장소</Link>
+          <Link href='/places'>장소 목록</Link>
+          <Link href='/place/1234'>장소 상세</Link>
+          <Link href='/review/1234'>리뷰 상세</Link>
+          <Link href='/review/write'>리뷰 작성</Link>
+          <Link href='/login'>로그인</Link>
+          <Link href='/signup'>회원가입</Link>
+          <Link href='/user/1234'>마이 페이지</Link>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
