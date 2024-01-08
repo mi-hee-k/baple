37ce250cf98c4da42d0577b97f88d9cab17f5df8
hyewon-han <hw1635@hanmail.net>
2024-01-08 11:51:07 +0900

    init : 초기 세팅

---
+++ src/libs/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
