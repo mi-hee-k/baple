d50459d1ac028495ab1b905e294cc59fca00cc72
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-09 14:53:12 +0900

    add : class 생성자 추가, 댓글 추가 로직 추가(#10)
    mutation과 invalidateQueries 사용

--- src/components/review_details/CommentInput.tsx
+++ src/components/review_details/CommentInput.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import { Input } from '@nextui-org/react';
+import React, { useState } from 'react';
+import { Button, Input } from '@nextui-org/react';
+import newComment from '@/utils/newComment';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { insertNewComment } from '@/apis/comments';
 
-const CommentInput = () => {
+interface Props {
+  reviewId: string;
+}
+
+const CommentInput = ({ reviewId }: Props) => {
+  const [comment, setComment] = useState('');
+
+  const USER_ID = '05a23d3d-a573-4f95-b7af-6b03779eb74b'; //AUTH 생길때까지 임시로 하드코딩
+
+  const queryClient = useQueryClient();
+  const InsertMutate = useMutation({
+    mutationFn: insertNewComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['comments'] });
+    },
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('연결됨');
+
+    // 새로운 댓글 데이터 생성
+    const newCommentData = new newComment(reviewId, USER_ID, comment);
+
+    InsertMutate.mutate(newCommentData);
+
+    setComment('');
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <p>댓글</p>
-      <Input type='text' placeholder='댓글을 입력해 주세요' />
-    </div>
+      <Input
+        type='text'
+        placeholder='댓글을 입력해 주세요'
+        value={comment}
+        onChange={(e) => {
+          setComment(e.target.value);
+        }}
+      />
+      <Button color='primary' variant='ghost' type='submit'>
+        등록
+      </Button>
+    </form>
   );
 };
 
