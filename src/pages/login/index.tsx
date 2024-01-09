808808171e8f6951d250641a5082bb2172fac022
hyewon-han <hw1635@hanmail.net>
2024-01-09 18:00:03 +0900

    update : 회원가입 기능 구현 (#4)

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,46 +1,81 @@
-import { Input } from '@nextui-org/react';
+import { Button, Input } from '@nextui-org/react';
 import { EyeFilledIcon } from '@/components/login/EyeFilledIcon';
 import { EyeSlashFilledIcon } from '@/components/login/EyeSlashFilledIcon';
-import React from 'react';
+import React, { useState } from 'react';
 import Seo from '@/components/Seo';
 
 const LoginPage = () => {
-  const [isVisible, setIsVisible] = React.useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(() => {
+      const newEmail = e.target.value;
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!newEmail) setEmailError('이메일 아이디를 입력해주세요.');
+      else if (!emailRegex.test(newEmail))
+        setEmailError('올바른 이메일 형식이 아닙니다.');
+      else setEmailError('');
+      return newEmail;
+    });
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(() => {
+      const newPassword = e.target.value;
+      if (!newPassword) setPasswordError('비밀번호를 입력해주세요.');
+      else setPasswordError('');
+      return newPassword;
+    });
+  };
 
   const toggleVisibility = () => setIsVisible(!isVisible);
+
   return (
     <>
       <Seo title='Login' />
-      <div className=' h-screen flex flex-col justify-center items-center '>
-        <Input
-          isClearable
-          type='email'
-          label='Email'
-          variant='bordered'
-          placeholder='Enter your email'
-          onClear={() => console.log('input cleared')}
-          className='max-w-xs'
-        />
-        <Input
-          label='Password'
-          variant='bordered'
-          placeholder='Enter your password'
-          endContent={
-            <button
-              className='focus:outline-none'
-              type='button'
-              onClick={toggleVisibility}
-            >
-              {isVisible ? (
-                <EyeSlashFilledIcon className='text-2xl text-default-400 pointer-events-none' />
-              ) : (
-                <EyeFilledIcon className='text-2xl text-default-400 pointer-events-none' />
-              )}
-            </button>
-          }
-          type={isVisible ? 'text' : 'password'}
-          className='max-w-xs'
-        />
+      <div className='h-screen flex flex-col justify-center items-center gap-2'>
+        <form>
+          <Input
+            isClearable
+            type='email'
+            label='Email'
+            variant='bordered'
+            placeholder='이메일 아이디를 입력해주세요'
+            onClear={() => setEmail('')}
+            className='max-w-xs'
+            onChange={onChangeEmail}
+            value={email}
+          />
+          <div className='text-red-500 text-center'>{emailError}</div>
+          <Input
+            label='Password'
+            variant='bordered'
+            placeholder='비밀번호를 입력해주세요'
+            endContent={
+              <button
+                className='focus:outline-none'
+                type='button'
+                onClick={toggleVisibility}
+              >
+                {isVisible ? (
+                  <EyeSlashFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+                ) : (
+                  <EyeFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+                )}
+              </button>
+            }
+            type={isVisible ? 'text' : 'password'}
+            className='max-w-xs'
+            onChange={onChangePassword}
+            value={password}
+          />
+          <div className='text-red-500 text-center'>{passwordError}</div>
+          <Button color='primary'>Login</Button>
+        </form>
       </div>
     </>
   );
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,166 @@
-import React from "react";
+import Seo from '@/components/Seo';
+import { Button, Input } from '@nextui-org/react';
+import { EyeFilledIcon } from '@/components/login/EyeFilledIcon';
+import { EyeSlashFilledIcon } from '@/components/login/EyeSlashFilledIcon';
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { supabase } from '@/libs/supabase';
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const [isVisible, setIsVisible] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const [nicknameError, setNicknameError] = useState('');
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(() => {
+      const newEmail = e.target.value;
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!newEmail) setEmailError('이메일 아이디를 입력해주세요.');
+      else if (!emailRegex.test(newEmail))
+        setEmailError('올바른 이메일 형식이 아닙니다.');
+      else setEmailError('');
+      return newEmail;
+    });
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(() => {
+      const newPassword = e.target.value;
+      if (!newPassword) setPasswordError('비밀번호를 입력해주세요.');
+      else setPasswordError('');
+      return newPassword;
+    });
+  };
+
+  const onChangeConfirmPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(() => {
+      const newConfirmPassword = e.target.value;
+      if (!newConfirmPassword)
+        setConfirmPasswordError('비밀번호 확인을 입력해주세요.');
+      else if (password !== newConfirmPassword)
+        setConfirmPasswordError('비밀번호를 잘못 입력하셨습니다.');
+      else setConfirmPasswordError('');
+      return newConfirmPassword;
+    });
+  };
+
+  const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(() => {
+      const newNickname = e.target.value;
+      if (!newNickname) setNicknameError('닉네임을 입력해주세요.');
+      else if (newNickname.length < 2)
+        setNicknameError('닉네임은 2자 이상이어야 합니다.');
+      else setNicknameError('');
+      return newNickname;
+    });
+  };
+  console.log(email, password, confirmPassword, nickname);
+  const signUpHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          nickname,
+          avatar_url: null,
+        },
+      },
+    });
+    console.log('data', data);
+    if (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <>
+      <Seo title='SignUp' />
+      <div className='h-screen flex flex-col justify-center items-center gap-2'>
+        <form onSubmit={signUpHandler}>
+          <Input
+            type='email'
+            label='이메일'
+            variant='bordered'
+            placeholder='이메일 아이디를 입력해주세요'
+            onClear={() => setEmail('')}
+            className='max-w-xs'
+            onChange={onChangeEmail}
+            value={email}
+          />
+          <div className='text-red-500 text-center'>{emailError}</div>
+          <Input
+            label='비밀번호'
+            variant='bordered'
+            placeholder='비밀번호를 입력해주세요'
+            endContent={
+              <button
+                className='focus:outline-none'
+                type='button'
+                onClick={toggleVisibility}
+              >
+                {isVisible ? (
+                  <EyeSlashFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+                ) : (
+                  <EyeFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+                )}
+              </button>
+            }
+            type={isVisible ? 'text' : 'password'}
+            className='max-w-xs'
+            onChange={onChangePassword}
+            value={password}
+          />
+          <div className='text-red-500 text-center'>{passwordError}</div>
+          <Input
+            label='비밀번호 확인'
+            variant='bordered'
+            placeholder='비밀번호는 영문, 숫자 포함 최소 8자 이상 입력해주세요'
+            endContent={
+              <button
+                className='focus:outline-none'
+                type='button'
+                onClick={toggleVisibility}
+              >
+                {isVisible ? (
+                  <EyeSlashFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+                ) : (
+                  <EyeFilledIcon className='text-2xl text-default-400 pointer-events-none' />
+                )}
+              </button>
+            }
+            type={isVisible ? 'text' : 'password'}
+            className='max-w-xs'
+            onChange={onChangeConfirmPassword}
+            value={confirmPassword}
+          />
+          <div className='text-red-500 text-center'>{confirmPasswordError}</div>
+          <Input
+            type='text'
+            label='닉네임'
+            variant='bordered'
+            placeholder='닉네임을 입력해주세요'
+            onClear={() => setNickname('')}
+            className='max-w-xs'
+            onChange={onChangeNickname}
+            value={nickname}
+          />
+          <div className='text-red-500 text-center'>{nicknameError}</div>
+          <Button color='primary' type='submit'>
+            회원 가입
+          </Button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SignupPage;
