0f599d8fcc6a8e1dda6dd3db36cefc3dfaf2dd55
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-11 13:03:19 +0900

    feat  : TuiEditor WIP

--- src/components/review_details/ReviewBody.tsx
+++ src/components/review_details/ReviewBody.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
 import type { Tables } from '@/types/supabase';
+import { Button, Spacer } from '@nextui-org/react';
+import dynamic from 'next/dynamic';
 
 interface Props {
   review: Tables<'reviews'>;
 }
 
+const NoSsrEditor = dynamic(() => import('../common/TuiEditor'), {
+  ssr: false,
+});
+
 const ReviewBody = ({ review }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState('');
   const { id, created_at, content, user_id, place_id } = review;
+
+  console.log(editValue);
+  const ref = useRef<any>(null);
 
   return (
     <>
-      <p>{content}</p>
+      {!isEditing && <p>{content}</p>}
+      {isEditing && <NoSsrEditor content={content} editorRef={ref} />}
+      <Button
+        onClick={() => {
+          setIsEditing((prev) => !prev);
+        }}
+      >
+        수정state토글
+      </Button>
+      <Spacer y={10} />
       <p>placeid:{place_id}</p>
       <p>userid:{user_id}</p>
     </>
