cf9561c7f79bec65a5b8e8c17ff17ea000130847
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-12 11:51:43 +0900

    add : 댓글 input 에 react-hook-form 도입 시도(#21)

--- src/components/review_details/CommentInput.tsx
+++ src/components/review_details/CommentInput.tsx
@@ -5,12 +5,24 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { insertNewComment } from '@/apis/comments';
 import { toast } from 'react-toastify';
 import { USER_ID } from '@/constants/temp_develop';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/config/configStore';
+import { useForm } from 'react-hook-form';
 
 interface Props {
   reviewId: string;
 }
 
 const CommentInput = ({ reviewId }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onSubmit' });
+  const { isLoggedIn } = useSelector((state: RootState) => state.auth);
+  // console.log('로그인됨?>>', isLoggedIn);
+  console.log('리액트훅폼 에러>>', errors);
+
   const [comment, setComment] = useState('');
 
   const queryClient = useQueryClient();
@@ -27,10 +39,21 @@ const CommentInput = ({ reviewId }: Props) => {
     },
   });
 
-  const submitComment = async (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('연결됨');
-
+  const submitComment = async () => {
+    // e.preventDefault();
+    if (!isLoggedIn) {
+      toast.warning('로그인 후 이용해 주세요', {
+        position: 'top-right',
+        autoClose: 2000,
+        progress: undefined,
+        theme: 'light',
+      });
+      return;
+    }
+    // if (!comment || comment === '') {
+    //   alert('뭐라도 입력하세요!');
+    //   return;
+    // }
     // 새로운 댓글 데이터 생성
     const newCommentData = new newComment(reviewId, USER_ID, comment);
 
@@ -39,14 +62,29 @@ const CommentInput = ({ reviewId }: Props) => {
     setComment('');
   };
 
+  const onError = (errors) => {
+    const errMsg = errors.comment.message;
+    toast.warning(errMsg, {
+      position: 'top-right',
+      autoClose: 2000,
+      progress: undefined,
+      theme: 'light',
+    });
+  };
+
   return (
-    <form onSubmit={submitComment}>
+    <div>
       <p>댓글</p>
-      <div className='flex gap-5 items-center'>
+      <form
+        className='flex gap-5 items-center'
+        onSubmit={handleSubmit(submitComment, onError)}
+      >
         <Input
+          {...register('comment', { required: '댓글의 내용이 없습니다' })}
           type='text'
           placeholder='댓글을 입력해 주세요'
           value={comment}
+          data-required
           onChange={(e) => {
             setComment(e.target.value);
           }}
@@ -59,8 +97,8 @@ const CommentInput = ({ reviewId }: Props) => {
         >
           등록
         </Button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
