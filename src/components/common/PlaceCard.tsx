49a7889a11c0ade8323f3cc395ff198a7a062d25
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-15 20:14:22 +0900

    style : minor css change

--- src/components/common/PlaceCard.tsx
+++ src/components/common/PlaceCard.tsx
@@ -20,13 +20,14 @@ const PlaceCard = ({ place }: Props) => {
         // key={index}
         isPressable
         onPress={() => router.push(`/place/${place.id}`)}
-        className='p-2 w-[250px] h-[240px]'
+        className='p-2 w-[250px] h-[250px]'
       >
         <CardBody className='overflow-visible p-0'>
           <Image
             shadow='sm'
             radius='lg'
             width='100%'
+            height={230}
             alt={place.place_name}
             className='w-full object-cover h-[140px]'
             src={imgURL || undefined}
