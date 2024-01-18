27c246707b86e05964c708eed131fe0413bcc64f
Jangkaeul <09autumn08@naver.com>
2024-01-18 12:52:42 +0900

    style: 리뷰 작성 페이지 css 수정 및 editor 기능 teaxarea 변경(#6)

--- src/pages/review/write/[placeId]/index.tsx
+++ src/pages/review/write/[placeId]/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState, ChangeEvent, useRef, useMemo } from 'react';
-import { Button, Spacer } from '@nextui-org/react';
+import React, { useState, ChangeEvent } from 'react';
+import { Button, Spacer, Textarea, Input } from '@nextui-org/react';
+import { useParams } from 'next/navigation';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/config/configStore';
 import {
@@ -9,20 +10,19 @@ import {
   useQueryClient,
 } from '@tanstack/react-query';
 import { insertNewReview } from '@/apis/reviews';
+import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { supabase } from '@/libs/supabase';
 import { toastSuccess, toastWarn } from '@/libs/toastifyAlert';
 import { getPlaceInfo } from '@/apis/places';
-import dynamic from 'next/dynamic';
 
 const ReviewWritePage = () => {
-  const [editorContent, setEditorContent] = useState('');
+  const [reviewText, setReviewText] = useState('');
   const [selectedImages, setSelectedImages] = useState<
     { file: File; imageUrl: string }[]
   >([]);
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const editorRef = useRef<any>(null); // TuiEditor의 ref를 설정
   const { userId } = useSelector((state: RootState) => state.auth);
   const router = useRouter();
   const { placeId } = router.query;
@@ -33,19 +33,12 @@ const ReviewWritePage = () => {
     // staleTime: Infinity,
   });
 
-  const NoSsrEditor = dynamic(
-    () => import('../../../../components/common/TuiEditor'),
-    {
-      ssr: false,
-    },
-  );
+  console.log('placeInfo', placeInfo);
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
-
     const selectedImageArray = [...selectedImages];
     const selectedFileArray = [...selectedFiles];
-
     if (files) {
       console.log('files', files);
       for (let i = 0; i < files.length; i++) {
@@ -62,8 +55,6 @@ const ReviewWritePage = () => {
           toastWarn('이미지는 최대 5장까지만 업로드 가능합니다.');
         }
       }
-      const editorContentValue = editorRef.current?.getInstance().getMarkdown();
-      setEditorContent(editorContentValue);
       setSelectedImages(selectedImageArray);
       setSelectedFiles(selectedFileArray);
     }
@@ -91,18 +82,6 @@ const ReviewWritePage = () => {
   };
 
   const onSubmitReview = async () => {
-    const editorIns = editorRef?.current?.getInstance();
-    const editValue = editorIns?.getMarkdown();
-    const isWhitespaceOnly = /^\s*$/;
-    console.log(editValue);
-    if (!editValue) {
-      toastWarn('내용을 입력하고 등록해 주세요');
-      return;
-    } else if (isWhitespaceOnly.test(editValue)) {
-      toastWarn('공백 이외의 내용을 입력해 주세요');
-      return;
-    }
-
     const publicUrlList: string[] = [];
     if (selectedFiles) {
       for (const file of selectedFiles) {
@@ -123,82 +102,84 @@ const ReviewWritePage = () => {
     if (placeInfo.image_url === null) {
     }
     const args = {
-      content: editValue,
+      content: reviewText,
       placeId: placeId as string,
       userId,
       publicUrlList,
     };
-
     mutate(args);
-
-    toastSuccess('등록되었습니다.');
+    toastSuccess('리뷰가 등록되었습니다.');
+    setReviewText('');
     router.replace(`/place/${placeId}`);
   };
 
-  const memoizedEditor = useMemo(
-    () => <NoSsrEditor content={editorContent} editorRef={editorRef} />,
-    [editorContent, NoSsrEditor],
-  );
-
   return (
-    <div>
+    <div className='p-10 max-w-screen-md mx-auto'>
+      <div>{placeInfo.place_name}</div>
       <div>
-        <div className='p-10 max-w-screen-md mx-auto'>
-          <div>{placeInfo.place_name}</div>
-          <h2 className='text-2xl font-bold mb-4'>
-            사진 올리기(최대 5장까지 가능합니다)
-          </h2>
-          <div className='mb-20 flex gap-6 mt-7'>
-            <label className='relative cursor-pointer'>
-              <input
-                type='file'
-                accept='image/*'
-                multiple
-                onChange={handleImageChange}
-                className='hidden'
-              />
-              <div className='w-24 h-24 bg-gray-200 flex items-center justify-center rounded'>
-                <span className='text-3xl'>+</span>
-              </div>
-            </label>
-            {selectedImages.map((image, index) => (
-              <div
-                key={index}
-                className='image-preview relative inline-block w-24 h-24'
-              >
-                <Image
-                  src={image.imageUrl}
-                  alt={`Selected Image ${index}`}
-                  fill
-                  style={{ objectFit: 'cover' }}
-                />
-                <button
-                  onClick={() => handleRemoveImage(index)}
-                  className='delete-button absolute font-bold -top-5 right-0 text-red-400 hover:text-red-800 cursor-pointer'
-                >
-                  x
-                </button>
-              </div>
-            ))}
-          </div>
-          <div className='flex items-center mb-8'>
-            <h2 className='text-xl font-bold mr-1'>후기</h2>
-            <span className='text-red-500 text-2xl font-bold'>*</span>
-          </div>
-          <div className='mb-7'>{memoizedEditor}</div>
-          <div className='flex itmes-center justify-center'>
-            <Spacer x={2} />
-            <Button
-              color='primary'
-              variant='solid'
-              className='px-8'
-              onClick={onSubmitReview}
-              // isDisabled={false}
-            >
-              등록하기
-            </Button>
-          </div>
+        <div className='flex items-center mb-8'>
+          <h2 className='text-2xl mr-1 mb-2'>후기</h2>
+          <span className='text-red-500 text-3xl font-bold'>*</span>
         </div>
+      </div>
+      <div className='mb-20'>
+        <Textarea
+          value={reviewText}
+          onChange={(event) => setReviewText(event.target.value)}
+          placeholder='다녀오신 장소에서 즐거운 시간을 보내셨나요? 
+          방문 경험을 사용자들과 공유해 주세요!
+          
+          리뷰를 보는 사용자를 위해 욕설, 비방, 명예훼손성 표현은 주의해 주세요.'
+          className='w-full p-2 border rounded focus:outline-none focus:border-blue-500'
+        />
+      </div>
+      <h2 className='text-xl  mb-4'>
+        사진 올리기(최대 5장까지 첨부 가능합니다)
+      </h2>
+      <div className='mb-20 flex gap-6 mt-7'>
+        <label className='relative cursor-pointer'>
+          <input
+            type='file'
+            accept='image/*'
+            multiple
+            onChange={handleImageChange}
+            className='hidden'
+          />
+          <div className='w-24 h-24 bg-gray-200 flex items-center justify-center rounded'>
+            <span className='text-3xl'>+</span>
+          </div>
+        </label>
+        {selectedImages.map((image, index) => (
+          <div
+            key={index}
+            className='image-preview relative inline-block w-24 h-24'
+          >
+            <Image
+              src={image.imageUrl}
+              alt={`Selected Image ${index}`}
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+            <button
+              onClick={() => handleRemoveImage(index)}
+              className='delete-button absolute font-bold -top-5 right-0 text-red-400 hover:text-red-800 cursor-pointer'
+            >
+              x
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className='flex itmes-center justify-center'>
+        <Spacer x={2} />
+        <Button
+          color='primary'
+          variant='solid'
+          className='px-8'
+          onClick={onSubmitReview}
+          isDisabled={!reviewText}
+        >
+          등록하기
+        </Button>
       </div>
     </div>
   );
