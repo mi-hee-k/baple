133caabbd866846c1f1e5cd1013bdbc9b4d62554
HIITMEMARIO <alswnsgur@nate.com>
2024-01-21 15:58:35 +0900

    수정중

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&libraries=services,clusterer&autoload=false`}
           strategy='beforeInteractive'
         />
       </body>
