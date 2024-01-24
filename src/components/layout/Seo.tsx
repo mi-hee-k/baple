db7dfa416965befd8b813a628a17c0c989203925
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2024-01-24 15:11:11 +0900

    update : seo에 오픈그래프 추가(#104)

--- src/components/layout/Seo.tsx
+++ src/components/layout/Seo.tsx
@@ -9,6 +9,13 @@ const Seo = ({ title }: Props) => {
   return (
     <Head>
       <title>{title} | BAPLE</title>
+      <meta property='og:title' content='Baple' />
+      <meta property='og:description' content='Baple' />
+      <meta property='og:url' content='https://baple.vercel.app/' />
+      <meta
+        property='og:image'
+        content='https://dummyimage.com/1200x400/b8b8b8/fff&text=BAPLE'
+      />
     </Head>
   );
 };
