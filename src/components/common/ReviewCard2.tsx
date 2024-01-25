b42fe3f104f4dd6d7a95fba99b9de284140d07dd
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-25 16:11:02 +0900

    style : 반응형 CSS WIP (#95)

--- src/components/common/ReviewCard2.tsx
+++ src/components/common/ReviewCard2.tsx
@@ -5,6 +5,7 @@ import { formatDate } from '@/utils/dateFormatter';
 import Image from 'next/image';
 import type { ReviewsFromRPC } from '@/types/types';
 import { useRouter } from 'next/router';
+import { useViewport } from '@/hooks/useViewport';
 
 interface Props {
   review: ReviewsFromRPC;
@@ -23,20 +24,26 @@ const ReviewCard2 = ({ review }: Props) => {
     user_name,
     user_avatar_url,
   } = review;
+  const { isTablet } = useViewport();
 
   return (
     <Link className='w-full' href={`/review/${unique_review_id}`}>
       <div className='card w-full  md:pr-[47px] py-[18px] p-2 border rounded-xl transition-all'>
         <div className='flex justify-between gap-x-4'>
-          <section className='flex flex-col w-[15%] gap-y-2 min-w-[118px] items-center justify-center'>
-            <Avatar
-              showFallback
-              src={user_avatar_url}
-              className='w-[65px] h-[65px]'
-            />
-            <strong className='text-[15px]'>{user_name}</strong>
-          </section>
-          <section className='flex flex-col sm:w-[60%] w-[50%]'>
+          {isTablet ? (
+            ''
+          ) : (
+            <section className='flex flex-col w-[15%] gap-y-2 min-w-[118px] items-center justify-center'>
+              <Avatar
+                showFallback
+                src={user_avatar_url}
+                className='w-[65px] h-[65px]'
+              />
+              <strong className='text-[15px]'>{user_name}</strong>
+            </section>
+          )}
+
+          <section className='flex flex-col md:w-[60%] w-[80%]'>
             <div className='flex flex-col gap-4'>
               <div className='flex gap-x-1'>
                 {images_url?.map((url) => (
@@ -46,7 +53,7 @@ const ReviewCard2 = ({ review }: Props) => {
                     src={url}
                     height={100}
                     width={100}
-                    className='sm:w-[100px] sm:h-[100px] transition-all h-[70px] w-[70px] object-cover object-center rounded-md'
+                    className='w-[100px] h-[100px] transition-all object-cover object-center rounded-md'
                   />
                 ))}
               </div>
