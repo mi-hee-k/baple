0501e715668ac82b190e857f9feef879a5f23858
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-26 17:58:48 +0900

    Update ThemeSwitcher.tsx

--- src/components/layout/ThemeSwitcher.tsx
+++ src/components/layout/ThemeSwitcher.tsx
@@ -1,11 +1,10 @@
 import { useTheme } from 'next-themes';
 import React, { useEffect, useState } from 'react';
 import { Switch } from '@nextui-org/react';
-import { Tooltip } from '@nextui-org/react';
 
 const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { setTheme } = useTheme();
   useEffect(() => {
     setMounted(true);
   }, []);
