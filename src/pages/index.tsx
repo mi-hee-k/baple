fec943d0249a22d930841e541c85ecd03cfc6401
hyewon-han <hw1635@hanmail.net>
2024-01-28 22:15:54 +0900

    bugfix : 장소 이동할 때 사이드바 닫히게 수정 (#101)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,47 +45,51 @@ const Home = ({ topBookmarked, topReviewed }: Props) => {
 
   const [localSearchValue, setLocalSearchValue] = useState('');
   return (
-    <div>
-      <Seo />
-      <Carousel
-        slideData={imgList} // imgList가 없으면 빈배열
-        slidesPerView={1} // 보여줄 슬라이스 수
-        slideHeight={'400px'} // 캐러셀 높이
-      />
-      <MainWrapper>
-        {/* 검색창 */}
-        <form
-          onSubmit={handleSearch}
-          className='flex justify-center w-full sm:w-[60%] m-auto mt-10 mb-4 sm:mb-8 bg-primary p-[2px] rounded-full overflow-hidden'
-        >
-          <input
-            placeholder='장소이름을 검색하세요'
-            value={localSearchValue}
-            onChange={(e) => setLocalSearchValue(e.target.value)}
-            className='rounded-full w-[80%] sm:w-full p-2 px-4 placeholder:text-md focus:outline-none'
+    <>
+      {isLoaded ? (
+        <div>
+          <Seo />
+          <Carousel
+            slideData={imgList} // imgList가 없으면 빈배열
+            slidesPerView={1} // 보여줄 슬라이스 수
+            slideHeight={'400px'} // 캐러셀 높이
           />
-          <Button
-            color='primary'
-            type='submit'
-            className='h-auto w-[20%] rounded-r-full'
-          >
-            <Image
-              src='/images/icons/search_white.svg'
-              width={24}
-              height={24}
-              alt='search_icon'
-            />
-          </Button>
-        </form>
-        <div className='flex flex-col w-full justify-center items-center'>
-          <MostReviews initialData={topReviewed} />
-          <Spacer y={10} />
-          <MostBookmarks initialData={topBookmarked} />
-          <Spacer y={20} />
+          <MainWrapper>
+            {/* 검색창 */}
+            <form
+              onSubmit={handleSearch}
+              className='flex justify-center w-full sm:w-[60%] m-auto mt-10 mb-4 sm:mb-8 bg-primary p-[2px] rounded-full overflow-hidden'
+            >
+              <input
+                placeholder='장소이름을 검색하세요'
+                value={localSearchValue}
+                onChange={(e) => setLocalSearchValue(e.target.value)}
+                className='rounded-full w-[80%] sm:w-full p-2 px-4 placeholder:text-md focus:outline-none'
+              />
+              <Button
+                color='primary'
+                type='submit'
+                className='h-auto w-[20%] rounded-r-full'
+              >
+                <Image
+                  src='/images/icons/search_white.svg'
+                  width={24}
+                  height={24}
+                  alt='search_icon'
+                />
+              </Button>
+            </form>
+            <div className='flex flex-col w-full justify-center items-center'>
+              <MostReviews initialData={topReviewed} />
+              <Spacer y={10} />
+              <MostBookmarks initialData={topBookmarked} />
+              <Spacer y={20} />
+            </div>
+          </MainWrapper>
+          <TopButton />
         </div>
-      </MainWrapper>
-      <TopButton />
-    </div>
+      ) : null}
+    </>
   );
 };
 
