f8e71f8b2397725c68b0f970314c166039a0ea3d
Jangkaeul <09autumn08@naver.com>
2024-01-28 16:04:08 +0900

    style: css 수정(#124)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export default function Custom404() {
   const router = useRouter();
@@ -13,12 +14,22 @@ export default function Custom404() {
   return (
     <div className='flex items-center justify-center h-screen'>
       <div className='text-center mx-4 md:mx-8 lg:mx-16 xl:mx-32'>
-        <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold mb-4 whitespace-normal animate-appearance-in text-purple'>
-          404 Error <br />
-        </h1>
-        <div className='mb-4 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
-          비정상적인 접근입니다.
+        <div className='flex items-center justify-center'>
+          <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold mb-4 whitespace-normal animate-appearance-in text-purple'>
+            404 Error <br />
+          </h1>
+          <div className='flex-shrink-0 mb-8'>
+            <Image
+              src='/images/icons/character.svg'
+              alt='character logo'
+              width={60} // 조절 가능
+              height={60} // 조절 가능
+            />
+          </div>
         </div>
+        {/* <div className='mb-4 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-2xl'>
+          비정상적인 접근입니다.
+        </div> */}
         <div>
           <p className='text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl whitespace-normal'>
             길을 잃으셨나요? 걱정 마세요, 함께 돌아가요 :)
