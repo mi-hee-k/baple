05a67f759a54cc708c950d7b8e90547e6837af3e
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-28 17:53:15 +0900

    style : nearby 페이지 스타일 적용

--- src/components/map/MylocationOverlayMap.tsx
+++ src/components/map/MylocationOverlayMap.tsx
@@ -10,7 +10,7 @@ const MylocationOverlayMap = ({ mylocation }: Props) => {
   return (
     <CustomOverlayMap position={mylocation.center} yAnchor={1.8} zIndex={2}>
       <div className=' w-[100px] bg-white flex justify-center rounded-md shadow-md h-[50px] items-center'>
-        <span className='text-lg font-bold'>현재위치!</span>
+        <span className='text-lg font-bold text-[#1e1e1e]'>현재위치!</span>
       </div>
     </CustomOverlayMap>
   );
