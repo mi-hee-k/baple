5d6dd5b653d49880aa739321eadb984734247053
hyewon-han <hw1635@hanmail.net>
2024-01-28 16:33:14 +0900

    style : 캐러셀 오토플레이 true(#101)

--- src/components/home/MostReviews.tsx
+++ src/components/home/MostReviews.tsx
@@ -2,13 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { getTopReviewedPlaces } from '@/apis/places';
 import PlaceCard from '../common/PlaceCard';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules';
+import { Navigation, Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
 import { Spacer } from '@nextui-org/react';
 import { useViewport } from '@/hooks/useViewport';
-
 import type { PlacesForPlaceCard, PlacesForSearch } from '@/types/types';
 import PlaceCard3 from '../common/PlaceCard3';
 
@@ -44,7 +43,7 @@ const MostReviews = ({ initialData }: Props) => {
         slidesPerView={isMobile ? 1 : isTablet ? 2 : 4}
         navigation={true} // prev, next button
         modules={[Navigation, Autoplay]}
-        // autoplay={true}
+        autoplay={true}
         className=''
         allowTouchMove={false}
       >
