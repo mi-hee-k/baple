5a4791582d2f83cb09ab31230ff723dd453fc7b8
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2024-01-28 21:07:30 +0900

    chore :realtime 시도중

--- src/apis/commentAlert.ts
+++ src/apis/commentAlert.ts
@@ -2,12 +2,13 @@ import { supabase } from '@/libs/supabase';
 
 export const commentsAlert = async () => {
   await supabase
-    .channel('schema-db-changes')
+    .channel('comments-alarm')
     .on(
       'postgres_changes',
       {
         schema: 'public', // Subscribes to the "public" schema in Postgres
         event: '*', // Listen to all changes
+        table: 'comments',
       },
       (payload) => console.log(payload),
     )
