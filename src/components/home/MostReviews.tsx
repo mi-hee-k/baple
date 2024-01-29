75e0618236275fe3af2ab6a777f9f6500e01cfd3
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2024-01-29 14:59:31 +0900

    style: 메인페이지 placeCard 레이아웃 깨지는 것 수정(#104)

--- src/components/home/MostReviews.tsx
+++ src/components/home/MostReviews.tsx
@@ -28,6 +28,12 @@ const MostReviews = ({ initialData }: Props) => {
     return <p>데이터 불러오는중...</p>;
   }
 
+  const slidesPerCardView = () => {
+    if (isMobile) {
+      return 2;
+    }
+  };
+
   return (
     <div className='w-full '>
       <div className='flex flex-col gap-2'>
@@ -40,7 +46,7 @@ const MostReviews = ({ initialData }: Props) => {
       <Swiper
         loop={true} // 슬라이드 루프
         spaceBetween={5}
-        slidesPerView={isMobile ? 2 : isTablet ? 3 : 4}
+        slidesPerView={isMobile ? 2 : isTablet ? 2 : 4}
         navigation={true} // prev, next button
         modules={[Navigation, Autoplay]}
         autoplay={true}
