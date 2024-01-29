35cc69e5c0071839d6a0fb534776ee945e7e379d
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-01-29 20:25:55 +0900

    style : 스피너 모달에 theme style 적용(#167)

--- src/components/review_write/ReviewSubmitSpinner.tsx
+++ src/components/review_write/ReviewSubmitSpinner.tsx
@@ -1,15 +1,18 @@
 import { Spacer, Spinner } from '@nextui-org/react';
+import { useTheme } from 'next-themes';
 import React from 'react';
 
-// interface Props {
-//   isOpen: boolean;
-// }
 const ReviewSubmitSpinner = () => {
+  const { theme } = useTheme();
   return (
     <div
       className={`w-[100%] h-[100%] fixed  z-10 flex justify-center items-center`}
     >
-      <div className='z-20 bg-white rounded-md w-[300px] h-[200px] flex flex-col justify-center items-center opacity-100 absolute bg-opacity-100'>
+      <div
+        className={`z-20 bg-${
+          theme === 'baple' ? 'white' : 'secondary'
+        } rounded-md w-[300px] h-[200px] flex flex-col justify-center items-center opacity-100 absolute bg-opacity-100`}
+      >
         <Spinner size='lg' />
         <Spacer y={10} />
         <p>업로드중.. 잠시만 기다려주세요 😜</p>
