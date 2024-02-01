278fc8ec8d44634871cb0e6bbd278767065a5248
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-02-01 21:38:44 +0900

    Update dateFormatter.ts

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -2,8 +2,7 @@ export function formatDate(inputDate: Date | string) {
   const date = new Date(inputDate);
   const formattedDate = new Date(inputDate).toLocaleString('ko');
   if (formattedDate.length === 22) return `${formattedDate.slice(0, 19)}`;
-  if (formattedDate.length === 23) return `${formattedDate.slice(0, 20)}`;
-  else if (formattedDate.length === 22) return `${formattedDate.slice(0, 20)}`;
+  else if (formattedDate.length === 23) return `${formattedDate.slice(0, 20)}`;
   else if (formattedDate.length === 24) return `${formattedDate.slice(0, 21)}`;
   // const year = date.getFullYear().toString().slice(2);
   // const month = (date.getMonth() + 1).toString().padStart(2, '0');
