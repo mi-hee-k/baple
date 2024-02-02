0d67c6968d32b54d6c4461ed3240943ea6ca84d0
HIITMEMARIO <alswnsgur@nate.com>
2024-02-02 15:15:04 +0900

    feat:framermotion적용중

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,10 @@
 import MainWrapper from '@/components/layout/MainWrapper';
-import React from 'react';
+import React, { useRef } from 'react';
 import YouTube, { YouTubeProps } from 'react-youtube';
+import { motion, useScroll } from 'framer-motion';
 
 const AboutPage = () => {
+  const scrollRef = useRef(null);
   const onPlayerReady: YouTubeProps['onReady'] = (event) => {
     event.target.pauseVideo();
   };
@@ -15,22 +17,37 @@ const AboutPage = () => {
       autoplay: 1,
     },
   };
-
+  const { scrollYProgress } = useScroll();
   return (
     <MainWrapper>
       <div className='flex flex-col justify-center items-center'>
-        <section className='w-full bg-black h-[300px] '></section>
-        <section className='flex items-center justify-center flex-col w-[50%] mt-[100px]'>
-          <p className='text-[30px]'>Team Mission</p>
-          <p className='text-[20px]'>
-            Baple은 Barrier Free와 Best Place를 결합해, 교통 약자들을 위한
-            배리어 프리 서비스(장소)를 소개하는 플랫폼입니다. 우리의 목표는 모든
-            사람이 쉽게 이용할 수 있는 장소를 찾고 공유하며 더 나은 환경을
-            만들어가는 것입니다.
-          </p>
-        </section>
+        <motion.div
+          className='w-full bg-black h-[300px]'
+          initial={{ translateX: '1000px', opacity: 0 }}
+          animate={{ translateX: '0px', opacity: 1 }}
+          transition={{ duration: 0.9 }}
+        ></motion.div>
+        <motion.div
+          initial={{ translateX: '-1000px', opacity: 0 }}
+          animate={{ translateX: '0px', opacity: 1 }}
+          transition={{ duration: 0.9 }}
+          style={{ scaleX: scrollYProgress }}
+        >
+          <div
+            ref={scrollRef}
+            className='flex bg-slate-400 items-center justify-center flex-col  mt-[250px] w-full'
+          >
+            <p className='text-[30px]  font-bold'>Team Mission</p>
+            <p className='text-[20px] w-[50%] mt-[50px]'>
+              Baple은 Barrier Free와 Best Place를 결합해, 교통 약자들을 위한
+              배리어 프리 서비스(장소)를 소개하는 플랫폼입니다. 우리의 목표는
+              모든 사람이 쉽게 이용할 수 있는 장소를 찾고 공유하며 더 나은
+              환경을 만들어가는 것입니다.
+            </p>
+          </div>
+        </motion.div>
         <div className='flex flex-row '>
-          <YouTube
+          {/* <YouTube
             className='mt-[100px] mr-11'
             videoId='2g811Eo7K8U'
             opts={opts}
@@ -47,14 +64,36 @@ const AboutPage = () => {
             videoId='2g811Eo7K8U'
             opts={opts}
             onReady={onPlayerReady}
-          />
+          /> */}
         </div>
-        <p className='text-[30px] mt-[100px]'>장소 찾기와 공유하기</p>
-        <div className='flex flex-row mt-[100px] w-full justify-center'>
-          <div className='w-[30rem] h-[40rem] bg-black mr-[10rem]'></div>
-          <div className='w-[30rem] h-[40rem] bg-black'></div>
-        </div>
-        <p className='text-[30px] mt-[100px]'>원하는 편의 시설로 검색하기</p>
+        <motion.div
+          initial={{ translateX: '-1000px', opacity: 0 }}
+          animate={{ translateX: '0px', opacity: 1 }}
+          transition={{ duration: 0.9 }}
+          viewport={{ root: scrollRef }}
+        >
+          <div
+            ref={scrollRef}
+            className='text-[30px] mt-[300px] font-bold  bg-slate-400'
+          >
+            <p className='text-[30px] mt-[100px] font-bold flex justify-center bg-slate-400'>
+              장소 찾기와 공유하기
+            </p>
+            <div className='flex flex-row mt-[100px] w-full justify-center'>
+              <div className='w-[30rem] h-[40rem] bg-black mr-[10rem]'></div>
+              <div className='w-[30rem] h-[40rem] bg-black'></div>
+            </div>
+          </div>
+        </motion.div>
+        <p className='text-[30px] mt-[100px]  font-bold'>
+          원하는 편의 시설로 검색하기
+        </p>
+        <div className='w-full h-[30rem] bg-black'></div>
+        <p className='text-[30px] mt-[100px]  font-bold'>리뷰 남기기</p>
+        <div className='w-full h-[30rem] bg-black mt-10'></div>
+        <div className='w-full h-[30rem] bg-black mt-10'></div>
+        <p className='text-[30px] mt-[100px]  font-bold'>지금 시작하기</p>
+        <div className='w-full h-[30rem] bg-black mt-10'></div>
       </div>
     </MainWrapper>
   );
