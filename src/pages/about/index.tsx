789dd72a915552e871c21b8aae0cbb08b8e70a8a
HIITMEMARIO <alswnsgur@nate.com>
2024-02-04 17:58:01 +0900

    수정중

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,32 +1,12 @@
-import MainWrapper from '@/components/layout/MainWrapper';
-import React, { useRef } from 'react';
-import YouTube, { YouTubeProps } from 'react-youtube';
+import React from 'react';
 import { motion, useScroll } from 'framer-motion';
 import Image from 'next/image';
 import person from '../../../public/images/aboutPage/Group 1181.png';
-import macbook from '../../../public/images/aboutPage/MacBook Pro 23.png';
-import iphone from '../../../public/images/aboutPage/iPhone 15.png';
 import mainLogo from '../../../public/images/aboutPage/about_LOGO.png';
 import Carousel from '@/components/common/Carousel';
-import { Button, Spacer } from '@nextui-org/react';
+import YoutubeCard from '@/components/about/Youtube';
 
 const AboutPage = () => {
-  // const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-  //   event.target.pauseVideo();
-  // };
-
-  // const videoid = ['2g811Eo7K8U', 'dietV6QhYek', '2g811Eo7K8U'];
-
-  // const opts: YouTubeProps['opts'] = {
-  //   height: '390',
-  //   width: '400',
-  //   playerVars: {
-  //     autoplay: 1,
-  //     rel: 0, //관련 동영상 표시하지 않음 (근데 별로 쓸모 없는듯..)
-  //     modestbranding: 1, // 컨트롤 바에 youtube 로고를 표시하지 않음
-  //   },
-  // };
-
   const carouselData = [
     '/images/aboutPage/Carousel1.png',
     '/images/aboutPage/Carousel2.png',
@@ -36,87 +16,64 @@ const AboutPage = () => {
   ];
 
   return (
-    <div className='flex flex-col justify-center items-center overflow-hidden'>
-      {/* <motion.div
-        className='w-full bg-black h-[300px]'
-        initial={{ x: '-300px', opacity: 0 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: false }}
-        transition={{ ease: 'easeInOut', duration: 1 }}
-      ></motion.div>
-      <motion.div
-        initial={{ x: '300px', opacity: 0 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: false }}
-        transition={{ ease: 'easeInOut', duration: 1 }}
-        className='flex bg-slate-400 items-center justify-center flex-col  mt-[250px] w-full'
-      >
-        <p className='text-[30px]  font-bold'>Team Mission</p>
-        <p className='text-[20px] w-[50%] mt-[50px]'>
-          Baple은 Barrier Free와 Best Place를 결합해, 교통 약자들을 위한 배리어
-          프리 서비스(장소)를 소개하는 플랫폼입니다. 우리의 목표는 모든 사람이
-          쉽게 이용할 수 있는 장소를 찾고 공유하며 더 나은 환경을 만들어가는
-          것입니다.
-        </p>
-      </motion.div>
-      <motion.div
-        className='flex flex-row mt-[200px]'
-        initial={{ x: '-300px', opacity: 0 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: false }}
-        transition={{ ease: 'easeInOut', duration: 1 }}
-      >
-        {videoid.map((item) => {
-            return (
-              <YouTube
-                className='mt-[100px] mr-11'
-                videoId={item}
-                opts={opts}
-                onReady={onPlayerReady}
-                key={item.length}
-              />
-            );
-          })}
-        <YouTube
-          className='mt-[100px] mr-11'
-          videoId='2g811Eo7K8U'
-          opts={opts}
-          onReady={onPlayerReady}
-        />
-        
-      </motion.div>
-        */}
-
+    <div className='w-full flex flex-col justify-center items-center overflow-hidden'>
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: false }}
-        transition={{ duration: 1 }}
-        className='h-[60rem] w-full justify-center flex'
+        transition={{ duration: 2 }}
+        className='h-[60rem] w-full items-center flex-col flex'
       >
         <Image
           src={mainLogo}
           alt='이미지'
-          className='relative h-[162px] z-10 mt-12'
+          className='relative sm:h-[162px] sm:w-[400px] h-[80px] w-[200px] z-10 mt-[100px]'
         />
+        <p className='mt-[200px] text-[30px] font-extrabold'>Team Mission</p>
+        <p className='w-[60%] text-center mt-[80px] font-bold text-[20px] break-words'>
+          Baple은 Barrier Free와 Best Place를 결합해, 교통 약자들을 위한 배리어
+          프리 정보를 소개하는 플랫폼입니다. 우리의 목표는 모든 사람이 쉽게
+          이용할 수 있는 장소를 찾고 공유하며 더 나은 환경을 만들어가는
+          것입니다.
+        </p>
       </motion.div>
 
-      <div className='w-full h-[60rem]  bg-[#EFEAFF]'>
+      <div className='w-full h-[60rem] flex flex-col  items-center bg-[#EFEAFF]'>
         <motion.div
           initial={{ opacity: 0, scale: 0.7 }}
           whileInView={{ opacity: 1, scale: 1 }}
           viewport={{ once: false }}
           transition={{ duration: 1 }}
-          className='w-full flex flex-col justify-center items-center mt-[300px] '
-        ></motion.div>
+          className='w-full flex flex-col justify-center items-center  '
+        >
+          <YoutubeCard />
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: '200px' }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 1 }}
+          className='w-[90%] sm:w-[50%]  '
+        >
+          <p className='font-extrabold text-[30px] mt-[50px] mb-[10px]'>
+            Barrier free?
+          </p>
+          <p className='text-[20px] font-bold'>
+            배리어프리는 장벽을 뜻하는 배리어(Barrier)와 자유를 의미하는
+            프리(Free) 의 합성어로, 장애인과 노약자 등 다양한 사회적 약자들이
+            살기 좋은 사회를 만들기 위해 물리적, 제도적, 심리적 장벽을
+            허물고자하는 노력입니다.
+          </p>
+        </motion.div>
       </div>
 
       <motion.div
-        initial={{ x: '300px', opacity: 0 }}
+        initial={{ x: '-200px', opacity: 0 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: false }}
         transition={{ ease: 'easeInOut', duration: 1 }}
-        className='w-[50%] h-[60rem] pt-[10%]'
+        className='sm:w-[50%] sm:h-[60rem] md:w-[80%] md:h-full w-full h-full pt-[10%]'
         id='about'
       >
         <Carousel
@@ -126,63 +83,61 @@ const AboutPage = () => {
         />
       </motion.div>
 
-      <div className='w-full flex flex-col justify-center items-center  '>
-        {/* <p className='text-[30px] mt-[100px]  font-bold'>지금 시작하기</p> */}
-        <div className='w-full h-[60rem] flex bg-[red]   relative  items-center justify-center'>
-          <div className=' flex w-[50%] h-[60rem]  left-[50%]'>
-            <motion.div
-              initial={{ opacity: 0, y: '-300px' }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: false }}
-              transition={{ duration: 1 }}
-              className='absolute top-[8%] right-[25%]'
-            >
-              <Image src={macbook} alt='이미지' className='relative z-1' />
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0, x: '-1000px' }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: false }}
-              transition={{ duration: 1 }}
-              className='absolute top-[45%] left-[20%] bg-black w-[100%] h-[100%]'
-            >
-              <Image src={iphone} alt='이미지' className='relative z-10' />
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0, x: '100px' }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: false }}
-              transition={{ duration: 1 }}
-              className='absolute top-[30%] right-[5%]'
-            >
-              <div className='ml-[10rem] relative top-[-50px]'>
-                <p className='flex text-[30px] font-extrabold'>
-                  Barrier Free Place is &nbsp;
-                  <span className='text-primary'>Best Place</span>
-                </p>
-                <p className='font-bold'>
-                  당신을 위한 최고의 장소를 찾아보세요!
-                </p>
-              </div>
+      <div className='w-full h-[60rem] flex bg-no-repeat relative   justify-center    '>
+        <Image
+          src={'/images/aboutPage/LastSection.png'}
+          alt='이미지'
+          fill={true}
+        />
+        {/* <motion.div
+            initial={{ opacity: 0, y: '-300px' }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: false }}
+            transition={{ duration: 1 }}
+            className='absolute top-[8%] right-[25%]'
+          >
+            <Image src={macbook} alt='이미지' className='relative z-1' />
+          </motion.div> */}
+        {/* <motion.div
+            initial={{ opacity: 0, x: '-1000px' }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: false }}
+            transition={{ duration: 1 }}
+            className='absolute top-[45%] left-0 bg-black w-full h-[100%]'
+          >
+            <Image src={iphone} alt='이미지' className='relative z-10 left-0' />
+          </motion.div> */}
+        <motion.div
+          initial={{ opacity: 0, x: '100px' }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 1 }}
+          className='absolute top-[30%] right-[15%]'
+        >
+          {/* <div className='ml-[10rem] relative top-[-50px]'>
+            <p className='flex text-[30px] font-extrabold'>
+              Barrier Free Place is &nbsp;
+              <span className='text-primary'>Best Place</span>
+            </p>
+            <p className='font-bold'>당신을 위한 최고의 장소를 찾아보세요!</p>
+          </div> */}
 
-              <Image src={person} alt='이미지' className='relative z-10' />
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: false }}
-              transition={{ duration: 1 }}
-              className='absolute bottom-[10%] flex flex-col justify-center items-center left-[40%]'
-            >
-              <p className='text-[20px] font-extrabold'>
-                Baple은 PC에서도, 모바일 환경에서도 어디서나 함께합니다
-              </p>
-              <Button className='bg-primary mt-8 w-[20rem] rounded-xl font-bold text-[#fff]'>
-                Baple 시작하기
-              </Button>
-            </motion.div>
-          </div>
-        </div>
+          <Image src={person} alt='이미지' className='relative z-10' />
+        </motion.div>
+        {/* <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: false }}
+            transition={{ duration: 1 }}
+            className='absolute bottom-[10%] flex flex-col justify-center items-center left-[40%]'
+          >
+            <p className='text-[20px] font-extrabold'>
+              Baple은 PC에서도, 모바일 환경에서도 어디서나 함께합니다
+            </p>
+            <Button className='bg-primary mt-8 w-[20rem] rounded-xl font-bold text-[#fff]'>
+              Baple 시작하기
+            </Button>
+          </motion.div> */}
       </div>
     </div>
   );
